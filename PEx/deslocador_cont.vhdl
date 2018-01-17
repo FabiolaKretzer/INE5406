f8ea931daa85e72c7329937cc943cfcd431b75c7
Fabíola Maria Kretzer <fabiolakretzer@hotmail.com>
2018-01-16 22:20:37 -0200

    Update deslocador_cont.vhdl

--- PEx/deslocador_cont.vhdl
+++ PEx/deslocador_cont.vhdl
@@ -1,15 +1,28 @@
--- INSERT CODE HERE
-
---Nome do aluno: 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity deslocador_cont is
-    generic(width: integer := 8);
-    port(
-        inpt: in unsigned(width-1 downto 0);
-        op: in unsigned(1 downto 0);
-        outp: out unsigned(width-1 downto 0)
-    );
+  generic(width : integer:=8);
+  port(
+    inpt: in unsigned(width-1 downto 0);
+    op: in unsigned(1 downto 0);
+    oupt: out unsigned(width-1 downto 0)  
+  );
 end entity;
 
--- INSERT CODE HERE
+architecture deslocador of deslocador_cont is
+  signal deslocamento: unsigned(width-1 downto 0);
+  begin
+    process(deslocamento, inpt, op)
+    begin
+      for i in to_integer(op) to width-1 loop
+        deslocamento(i) <= inpt(i-to_integer(op));
+      end loop;
+      for i in 0 to (to_integer(op)-1) loop
+        deslocamento(i) <= '0';
+      end loop;
+      oupt <= deslocamento;
+    end process;
+end architecture;
 
